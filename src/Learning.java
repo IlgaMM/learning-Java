433746dd9805f5506f2b65ddb0f797f546ba761f
Momi.lv <ilga.miezaine@gmail.com>
2023-03-07 17:48:19 +0200

    String saisinajums saglaba tekstu

--- src/Learning.java
+++ src/Learning.java
@@ -1,2 +1,8 @@
 public class Learning {
+    public static void main(String[] args) {
+
+        System.out.println("Mani pirmie kodi");
+        String vards = "Ilga";
+        System.out.println(vards);
+    }
 }
